e14014b3f2395eb8b3ce76fde53b57ef98d07ce4
Flatiron School <flatiron@Flatirons-MacBook-Air-2.local>
2019-06-25 15:59:26 -0700

    Detect real day of week

--- dow.py
+++ dow.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
 """Tells us the day of the week."""
 
-print("It's Tuesday!")
+from datetime import datetime
+import calendar
+DAYS = list(calendar.day_name)
 
+def find_day_if_week():
+    """Returns the day of week as a string, e.g. 'Monday'"""
+    now = datetime.now()
+    weekday = now.weekday()
+    return DAYS[weekday]
+    
 
+def main():
+    """Sends the day of the week to stdout."""
+    day = find_day_if_week()
+    print(f"It's {day}!")
+    print("test")
+
+if __name__ == "__main__":
+    main()
